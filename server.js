d3a03b8ce0fa140deff65b59914f0e1e66372e28
Nathan Montelli <nathanmontelli@gmail.com>
2022-01-11 20:36:27 -0800

    added note routes and fixed servers

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const path = require('path')
 
+
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended:true }))
-app.use(expess.json())
+app.use(express.json())
 
-app.use(require('./routes/index.js'))
 app.use(require('./routes/noteRoutes.js'))
+app.use(require('./routes/index.js'))
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000
+app.listen(PORT)
